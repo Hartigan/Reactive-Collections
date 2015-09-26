3c5745cfd3f220e91bc820cf2a036f44a59b69f9
Anatoly Safonov <hartigans@live.com>
2015-09-26 20:37:59 +0300

    implemented ObservableSet

---
+++ src/ReactiveCollections/ReactiveCollections/Implementation/Collections/ObservableSet.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using JetBrains.Annotations;
+using ReactiveCollections.Abstract.Collections;
+using ReactiveCollections.Abstract.Transactions;
+using ReactiveCollections.Implementation.Transactions;
+
+namespace ReactiveCollections.Implementation.Collections
+{
+	public class ObservableSet<T> : IObservableSet<T>
+	{
+		[NotNull]
+		private readonly List<T> _set;
+
+		[NotNull]
+		private readonly Subject<IEnumerable<IUpdateSetQuery<T>>> _subject;
+
+		[CanBeNull]
+		private Transaction<IUpdateSetQuery<T>, T> _transaction;
+
+		public ObservableSet()
+		{
+			_subject = new Subject<IEnumerable<IUpdateSetQuery<T>>>();
+			_set = new List<T>();
+		}
+
+		public ObservableSet(IEnumerable<T> items)
+		{
+			_subject = new Subject<IEnumerable<IUpdateSetQuery<T>>>();
+			_set = new List<T>(items);
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			return _set.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public IObservable<IEnumerable<IUpdateSetQuery<T>>> SetChanged
+		{
+			get { return _subject; }
+		}
+
+		public int Count
+		{
+			get { return _set.Count; }
+		}
+
+		public IDisposable Transaction()
+		{
+			if (_transaction != null)
+			{
+				throw new InvalidOperationException();
+			}
+
+			return _transaction = new Transaction<IUpdateSetQuery<T>, T>(_subject, DeleteTransaction);
+		}
+
+		public void Add(T item)
+		{
+			ToTransaction(UpdateSetQuery<T>.OnInsert(item));
+			_set.Add(item);
+		}
+
+		public bool Remove(T item)
+		{
+			if (_set.Remove(item))
+			{
+				ToTransaction(UpdateSetQuery<T>.OnRemove(item));
+				return true;
+			}
+			return false;
+		}
+
+		public void Clear()
+		{
+			ToTransaction(UpdateSetQuery<T>.OnClear(_set.ToList()));
+			_set.Clear();
+		}
+
+		private void DeleteTransaction()
+		{
+			_transaction = null;
+		}
+
+		private void ToTransaction([NotNull] IUpdateSetQuery<T> update)
+		{
+			if (_transaction != null)
+			{
+				_transaction.AddQuery(update);
+			}
+		}
+	}
+}
---
+++ src/ReactiveCollections/ReactiveCollections/Implementation/Transactions/Transaction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using ReactiveCollections.Abstract.Transactions;
+
+namespace ReactiveCollections.Implementation.Transactions
+{
+	internal class Transaction<T, TItem> : IDisposable where T : IUpdateSetQuery<TItem>
+	{
+		[NotNull]
+		private readonly IObserver<IEnumerable<T>> _observer;
+
+		[NotNull]
+		private readonly Action _callback;
+
+		[NotNull]
+		private readonly List<T> _queries = new List<T>();
+
+		public Transaction([NotNull] IObserver<IEnumerable<T>> observer, [NotNull] Action callback)
+		{
+			_observer = observer;
+			_callback = callback;
+		}
+
+		public void AddQuery(T query)
+		{
+			_queries.Add(query);
+		}
+
+		public void Dispose()
+		{
+			_observer.OnNext(_queries);
+			_callback();
+		}
+	}
+}
