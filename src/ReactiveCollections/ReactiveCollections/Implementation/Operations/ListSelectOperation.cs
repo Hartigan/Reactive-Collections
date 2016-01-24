2ec919e1622fb337116695733425f1131374873e
Anatoly Safonov <hartigans@live.com>
2016-01-24 13:47:39 +0300

    implement Select for List

---
+++ src/ReactiveCollections/ReactiveCollections/Implementation/Operations/ListSelectOperation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using ReactiveCollections.Abstract.Collections;
+using ReactiveCollections.Abstract.Transactions;
+using ReactiveCollections.Abstract.Transactions.Arguments;
+using ReactiveCollections.Implementation.Transactions;
+
+namespace ReactiveCollections.Implementation.Operations
+{
+	public class ListSelectOperation<TIn, TOut> : ListToListOperationBase<TIn, TOut>
+	{
+		[NotNull] private readonly Func<TIn, TOut> _selector;
+		[NotNull] private readonly List<TOut> _data = new List<TOut>(); 
+
+		public ListSelectOperation(
+			[NotNull] INotifyListChanged<TIn> source,
+			[NotNull] Func<TIn, TOut> selector) : base(source)
+		{
+			_selector = selector;
+		}
+
+		protected override IEnumerable<IUpdateListQuery<TOut>> OnInsert(IListOnInsertArgs<TIn> arg)
+		{
+			TOut newItem = _selector(arg.Item);
+			_data.Insert(arg.Index, newItem);
+			return new[] { UpdateListQuery<TOut>.OnInsert(newItem, arg.Index) };
+		}
+
+		public override IEnumerator<TOut> GetEnumerator()
+		{
+			return _data.GetEnumerator();
+		}
+
+		public override int Count => _data.Count;
+
+		public override TOut this[int index] => _data[index];
+
+		protected override IEnumerable<IUpdateListQuery<TOut>> OnEmpty(IListOnEmptyArgs arg)
+		{
+			return Enumerable.Empty<IUpdateListQuery<TOut>>();
+		}
+
+		protected override IEnumerable<IUpdateListQuery<TOut>> OnClear(IListOnClearArgs<TIn> arg)
+		{
+			var newArgs = UpdateListQuery<TOut>.OnClear(_data);
+			_data.Clear();
+			return new[] { newArgs };
+		}
+
+		protected override IEnumerable<IUpdateListQuery<TOut>> OnMove(IListOnMoveArgs<TIn> arg)
+		{
+			var item = _data[arg.OldIndex];
+			_data.RemoveAt(arg.OldIndex);
+			_data.Insert(arg.NewIndex, item);
+			return new[] { UpdateListQuery<TOut>.OnMove(item, arg.OldIndex, arg.NewIndex) };
+		}
+
+		protected override IEnumerable<IUpdateListQuery<TOut>> OnReplace(IListOnReplaceArgs<TIn> arg)
+		{
+			var newItem = _selector(arg.NewItem);
+			var oldItem = _data[arg.Index];
+			_data.RemoveAt(arg.Index);
+			_data.Insert(arg.Index, newItem);
+			return new[] { UpdateListQuery<TOut>.OnReplace(oldItem, newItem, arg.Index) };
+		}
+
+		protected override IEnumerable<IUpdateListQuery<TOut>> OnRemove(IListOnRemoveArgs<TIn> arg)
+		{
+			var item = _data[arg.Index];
+			_data.RemoveAt(arg.Index);
+			return new[] { UpdateListQuery<TOut>.OnRemove(item, arg.Index) };
+		}
+	}
+}
